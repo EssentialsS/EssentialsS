7363e5bfd9d9d9335a216677b760438b9681b6a4
mosemister <nekronmoseley@gmail.com>
2023-02-03 15:45:16 +0000

    Pom Setup

---
+++ src/main/java/org/essentialss/implementation/EssentialsSMain.java
@@ -0,0 +1,36 @@
+package org.essentialss.implementation;
+
+import com.google.inject.Inject;
+import net.kyori.adventure.identity.Identity;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.LinearComponents;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.api.Server;
+import org.spongepowered.api.command.Command;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.parameter.Parameter;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.lifecycle.ConstructPluginEvent;
+import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
+import org.spongepowered.api.event.lifecycle.StartingEngineEvent;
+import org.spongepowered.api.event.lifecycle.StoppingEngineEvent;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.builtin.jvm.Plugin;
+
+@Plugin("essentials-s")
+public class EssentialsSMain {
+
+    private final PluginContainer container;
+    private final Logger logger;
+
+    @Inject
+    public EssentialsSMain(final PluginContainer container, final Logger logger) {
+        this.container = container;
+        this.logger = logger;
+    }
+
+
+}
